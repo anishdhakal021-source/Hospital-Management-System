544b564077c8d93069895f3a57d1489126568cd5
Aamit-KC <kcammit@gmail.com>
2026-09-24 21:48:39 +0545

    Fixed login Problem

--- backend/apps/users/admin.py
+++ backend/apps/users/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import User
 
-# Register your models here.
 
-admin.site.register(User)
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ("Hospital Information", {"fields": ("role",)}),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Hospital Information", {"fields": ("role",)}),
+    )
